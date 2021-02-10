ab10f2efb1829ac6dbd3e8c50021ff3be75c7865
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2021-02-10 02:03:31 -0300

    solving-bugs

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# pokemon-pokedex
-A pokedex (pokemon) created with HTML, CSS and Javascript
+# Pokemon Pokedex
+
+In this project, I built a Pokedex from Pokemon.
+
+An API is consumed that gives the information of the Pokémon as name and type (fire, water, grass, etc.) and sends it to HTML.
+
+
+## Technologies
+In this project I used HTML5, CSS3 and JavaScript
+
+
+## Result
+
+> ### See and interact with the application [here](https://pokemon-pokedex.lusk1nha.vercel.app/) 
+[![codepen.io](/public/assets/pokedex.png)](https://pokemon-pokedex.lusk1nha.vercel.app/)
--- public/index.js
+++ public/index.js
@@ -8,12 +8,7 @@ const generations = {
     "1": { gen: 1, total: 1, name: "Kanto"},
     "2": { gen: 2, total: 152, name: "Johto"},
     "3": { gen: 3, total: 252, name: "Hoenn"},
-    "4": { gen: 4, total: 387, name: "Sinnoh"},
-    "5": { gen: 5, total: 494, name: "Unova"},
-    "6": { gen: 6, total: 650, name: "Kalos"},
-    "7": { gen: 7, total: 722, name: "Alola"},
-    "8": { gen: 8, total: 810, name: "Galar"},
-    "9": { gen: 9, total: 899, name: "Galar"}
+    "4": { gen: 4, total: 387, name: "Sinnoh"}
 }
 
 callingPokedex(1)
@@ -40,6 +35,8 @@ async function getPokemon(geneBegin, geneEnd) {
 
     // This code is the API consumption
     for ( let index = geneBegin.total; index < geneEnd.total; index++ ) {
+        if ( index === 350 ) index++ // ! the pokemon with id 350, was removed from api
+        
         const url = `https://pokeapi.co/api/v2/pokemon/${index}`
 
         const response = await fetch(url)
